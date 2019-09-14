e6f1d99b052b9baab950421b67cb6d113a4c5b8e
Никита Павленко <nick.pavlenko1998@gmail.com>
2019-09-14 18:13:34 +0300

    feat: Добавлена верстка для списка встреч, добавлены обработчики для активных и неактивных встреч

--- src/components/VotingList.js
+++ src/components/VotingList.js
@@ -1,18 +1,67 @@
 
 import React, { Component } from 'react';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import VotingMeetings from './ui/VotingMeetings'
 import TextField from '@material-ui/core/TextField';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
-export default class VotingList extends Component {
-    render() {
-        return (
-        <div className="voting-list-page">
-            Hello world
-        </div>
-        )
-    }
-}
+const List = [
+	{
+		meeting : 'Согласование переезда',
+        admin : 'Владимир Раменский',
+        meetType : 'Открытая',
+       // active: true
 
+	},
+	{
+		meeting : 'Обед',
+        admin : 'Владимир Раменский',
+        meetType : 'Закрытая',
+//        active: false
+	},
+	{
+        meeting : 'Планирование развития',
+        admin : 'Дмитрий Корчагин',
+        meetType : 'Открытая',
+  //      active: true
+	}
+
+];
+
+export default class VotingList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          title: 'Список голосований'
+        };
+      }
+           renderVotingMeetingsActive = itemList => {
+          return itemList.map(i => <VotingMeetings status={true} meeting={i.meeting} admin={i.admin} meetType={i.meetType}/>)
+        };
+
+        renderVotingMeetingsDisabled = itemList => {
+            return itemList.map(i => <VotingMeetings status={false} meeting={i.meeting} admin={i.admin} meetType={i.meetType}/>)
+          };
+
+    render() {
+        const store = this.store;
+        const {title} = this.state;
+        return (
+            <Container maxWidth="sm">
+            <div style={{ 'padding': '15px' }}>
+              <Typography variant="h4" gutterBottom>{title}</Typography>
+            </div>
+            <Divider />
+            {this.renderVotingMeetingsActive(List)}
+            {this.renderVotingMeetingsDisabled(List)}
+
+          </Container>
+        );
+      }
+    }
--- src/components/ui/VotingMeetings.js
+++ src/components/ui/VotingMeetings.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import Button from '@material-ui/core/Button';
-i
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import Typography from '@material-ui/core/Typography';
+import { string, number, boolean, shape } from 'prop-types';
 import axios from 'axios';
 
+
+
 export default class VotingList extends Component {
+    static propTypes={
+        meeting: string,
+        admin: string,
+        meetType: string,
+        status: boolean
+    };
+
     render() {
         return (
         <div className="voting-list__meetings">
-            <h1>Лист Голосоывния</h1>
-            <ExpansionPanel/>
+            <ExpansionPanel className='voting-list__meetings__item' defaultExpanded='true' disabled={this.props.status}>
+                <Typography className='item-name_first'>{this.props.meeting}</Typography>
+                <Typography className='item-name_second'>{this.props.admin}</Typography>
+                <Typography className='item-name_third'>{this.props.meetType}</Typography>
+            </ExpansionPanel>
         </div>
         )
     }
