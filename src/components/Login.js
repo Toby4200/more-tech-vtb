9bed77ea8d481a8425a6b113f8629f62276b9cbe
Melnik <Libroks@yandex.ru>
2019-09-14 19:33:08 +0300

    feat: Добавил кнопку из material-ui, выравнил input.

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,38 +5,33 @@ import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
-export default class LoginPage extends Component {
+
+export default class Login extends Component {
     render() {
     return (
       <div className="login-page">
         <div className="login-page__top">
           <h4 className="login-page__title">Голосование</h4>
           <span className="login-page__subtitle">Войдите в свой аккаунт</span>
-
-          <form className="form" onSubmit={ this.handleClick }>
-            <div className="form__wrapper">
-              <div className="form__container">
-                <TextField className="form__input" />
-                <TextField className="form__input" />
-                <TextField className="form__input" />
-              </div>
-
-              <div className="form__container">
-                <TextareaAutosize
-                  aria-label="empty textarea"
-                  placeholder="Empty"
-                  className="form__textarea"
-                  rows={ 8 }
-                />
-              </div>
-8            </div>
-
-            <input type="submit" value="Отправить" className="button"
-            />
-          </form>
-
         </div>
+          <form className="form">
+            <div className="form__wrapper">
+                <div className="form__container">
+                  <TextField className="form__input" />
+                  <TextField className="form__input" />
+                  <Button className="form__button" variant="contained" color="primary">
+                  Войти
+                </Button>
+                </div>
+              </div>
+
+          </form>
       </div>
     )
   }
 }
+
+
+
+
+
