eb2f96c0856c3f13f5e8d09307a9ac4497dd5663
Mike Yanshin <wh.prestige@gmail.com>
2019-09-14 14:24:58 +0300

    Создал станицы голосования,добавил header + footer

---
+++ src/components/votingNumberPage/votingNumberPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class votingNumberPage extends React.Component {
+
+  render() {
+    return(
+      <React.Fragment>
+        <header>
+          <title>
+            Страница созданя голосования
+          </title>
+        </header>
+
+        <footer>
+
+        </footer>
+      </React.Fragment>
+    )
+  }
+}
---
+++ src/components/votingPage/votingPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class votingNumberPage extends React.Component {
+
+  render() {
+    return(
+      <React.Fragment>
+        <header>
+          <title>
+            Страница голосованя
+          </title>
+        </header>
+
+        <footer>
+
+        </footer>
+      </React.Fragment>
+    )
+  }
+}
