5fb9f2b7c40f344767dfeb0d8990d1d42c93013c
Toba464 <ababin464@gmail.com>
2019-09-15 05:04:41 +0300

    feat: Моки юзера & fix факап с auth :)

--- mocks/server.js
+++ mocks/server.js
@@ -95,31 +95,101 @@ const init = async () => {
     }
   });
 
+  server.route({
+    method: 'GET',
+    path: '/voting/1',
+    handler: function (request, h) {
+      return [
+        {
+          id: 1,
+          name: 'Бабин Анатолий',
+          email: 'toba@klevertech.fun',
+          role: 'admin'
+        },
+        {
+          id: 2,
+          name: 'Петров Никита',
+          email: 'webnikler@klevertech.fun',
+          role: 'admin'
+        },
+        {
+          id: 3,
+          name: 'Панормов Никита',
+          email: 'npanormov@klevertech.fun',
+          role: 'user'
+        },
+        {
+          id: 4,
+          name: 'Мельник Михаил',
+          email: 'mmelnik@klevertech.fun',
+          role: 'user'
+        },
+        {
+          id: 5,
+          name: 'Яньшин Михаил',
+          email: 'myanshin@klevertech.fun',
+          role: 'user'
+        },
+        {
+          id: 6,
+          name: 'Василий Василькин',
+          email: 'vv@klevertech.fun',
+          role: 'user'
+        },
+        {
+          id: 7,
+          name: 'Николай Вахуемов',
+          email: 'nv@klevertech.fun',
+          role: 'user'
+        },
+        {
+          id: 8,
+          name: 'Тест Тестович',
+          email: 'tt@klevertech.fun',
+          role: 'user'
+        },
+        {
+          id: 9,
+          name: 'Бабин Анатолий',
+          email: 'xx@klevertech.fun',
+          role: 'user'
+        },
+        {
+          id: 10,
+          name: 'Бабин Анатолий',
+          email: 'zz@klevertech.fun',
+          role: 'user'
+        },
+      ]
+    }
+  });
+
+  // моки авторизации
+  server.route({
+    method: 'POST',
+    path: '/auth',
+    handler: function (request, h) {
+      switch (request.payload.username) {
+        case "toba@klevertech.fun" :
+          return {
+            role: "admin",
+          };
+        case "myanshin@klevertech.fun" :
+          return {
+            role: "user",
+          };
+      }
+
+      // Не авторизовывать если нет данных
+    }
+  });
+
+
+
   await server.start();
   console.log('Server running on %s', server.info.uri);
 
 };
-
-// rzd
-server.route({
-  method: 'POST',
-  path: '/auth',
-  handler: function (request, h) {
-    switch (request.payload.username) {
-      case "toba@klevertech.fun" :
-        return {
-          role: "admin",
-        };
-      case "myanshin@klevertech.fun" :
-        return {
-          role: "user",
-        };
-    }
-
-    // Не авторизовывать если нет данных
-  }
-});
-
 
 process.on('unhandledRejection', (err) => {
 
