1e2df24a0060d12c4c22e36fe7954be7e36e88e1
Mike Yanshin <wh.prestige@gmail.com>
2019-09-15 09:26:54 +0300

    доработка чата

--- src/components/VotingChat.js
+++ src/components/VotingChat.js
@@ -4,12 +4,45 @@ import ThumbUpIcon from "@material-ui/core/SvgIcon/SvgIcon";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import "../../src/styles/pages/_chat.scss"
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import InputBase from '@material-ui/core/InputBase'
 
 export default class VotingChat extends Component {
   render() {
     return (
       <div className="chat-card">
-        ТУТ ЧАТ
+        <div className="centrate">
+          <div className="message-plate">
+            <div className="points-tab"></div>
+
+            <div className="first-mess">
+              <div className="user-icon">
+                <AccountCircleIcon fontSize="large"/>
+              </div>
+              <div className="user-name">
+                Никита
+              </div>
+              <div className="user-message">
+                Отлично, что новый офис близко к метро!
+              </div>
+
+
+            </div>
+            <div className="second-message"></div>
+          </div>
+
+          <InputBase
+            id="standard-dense"
+            label="Ваше сообщение"
+            margin="dense"
+            multiline={true}
+          />
+          <div className="but">
+            <Button variant="contained" className="button">
+              Отправить сообщение
+            </Button>
+          </div>
+        </div>
       </div>
     )
   }
